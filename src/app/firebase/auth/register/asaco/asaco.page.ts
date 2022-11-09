7dd4e1c0e557e91baca17322fe1120c46675eb58
Navo1996 <gimhannavod75@gmail.com>
2022-11-09 21:48:41 +0530

    asacopage

--- src/app/firebase/auth/register/asaco/asaco.page.ts
+++ src/app/firebase/auth/register/asaco/asaco.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { MenuController, LoadingController } from '@ionic/angular';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { PasswordValidator } from 'src/app/validators/password.validator';
 
 @Component({
   selector: 'app-asaco',
@@ -9,8 +10,55 @@ import { MenuController, LoadingController } from '@ionic/angular';
   styleUrls: ['./asaco.page.scss'],
 })
 export class AsacoPage implements OnInit {
+  signupForm: FormGroup;
+  matching_passwords_group: FormGroup;
+  submitError: string;
+  redirectLoader: HTMLIonLoadingElement;
 
-  constructor() { }
+  validation_messages = {
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Enter a valid email.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 6 characters long.' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required' }
+    ],
+    'matching_passwords': [
+      { type: 'areNotEqual', message: 'Password mismatch' }
+    ]
+  };
+
+
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public menu: MenuController,
+    public loadingController: LoadingController,
+  ) { 
+
+     this.matching_passwords_group = new FormGroup({
+      'password': new FormControl('', Validators.compose([
+        Validators.minLength(6),
+        Validators.required
+      ])),
+      'confirm_password': new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areNotEqual(formGroup);
+    });
+
+    this.signupForm = new FormGroup({
+      'email': new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      'matching_passwords': this.matching_passwords_group
+    });
+
+  }
 
   ngOnInit() {
   }
