f400169d81606764fcb1b7dde85431c5d4038afa
Navo1996 <100310042+Navo1996@users.noreply.github.com>
2023-02-13 00:32:16 +0530

    css

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,5 +1,6 @@
 
-const config = {
+
+const Config = {
   apiKey: "AIzaSyA9Crwl93mXHXIFUFUUJ7JiuYdRcoUfgQ4",
   authDomain: "autobarber-9f0b9.firebaseapp.com",
   projectId: "autobarber-9f0b9",
@@ -8,4 +9,4 @@ const config = {
   appId: "1:500372370438:web:dbfc01771f544baff37f87"
 };
 
-export default config;
+export default Config;
